93983a71a28b29982b3f0b02bf4fb20acadd5dce
Tarsicio Carrizales <telecom.com.ve@gmail.com>
2022-07-03 13:59:20 -0400

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
 class DevelomentConfing(Config):
 	DEBUG = True	
 	SQLALCHEMY_DATABASE_URI ='sqlite:///python.sqlite'
-	SQLALCHEMY_DATABASE_URI_01 ='mysql://root:Panama.*@localhost/python'
+	SQLALCHEMY_DATABASE_URI_01 ='mysql://root:clave@localhost/python'
 
 config = {
 	'develoment': DevelomentConfing,
